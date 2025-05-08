6f716b1b943e3bd2036ecaf728c5b9bfa3662624
JingWen <jingwenhuang1008@gmail.com>
2025-05-08 11:38:35 +0800

    refactor: update, tested, 可以將Notepagejson解析成note

--- Refactor/noteMaker.js
+++ Refactor/noteMaker.js
@@ -6,21 +6,101 @@ class NoteMaker {
 
     switch (currentIndexItem.item.sourceType) {
       case model.SourceType.NOTION_PAGE_ID:
-        const notionJson = await this.fetchNotionPage(
+        const notionJson = await this.fetchNotionPageJson(
           //HACK: NOTION TOKEN 應該移轉到各自的資料庫ITEM項目
           currentIndexItem.item.sourceItem.apiToken,
           currentIndexItem.item.sourceItem.id
         );
-        console.log(notionJson);
+        const transformResult = await this.transformNotionPageJsonToNote(
+          notionJson
+        );
+        console.log(transformResult.Notes);
+        console.log(transformResult.WrongBlocks);
         break;
       default:
         break;
     }
   }
 
-  async makeNote({ index, item }) {}
+  //NOTE: 此函式高度依賴於NOTION本身的JSON結構
+  async transformNotionPageJsonToNote(notionPageJson) {
+    const Notes = [];
+    const WrongBlocks = [];
 
-  async fetchNotionPage(token, pageId) {
+    notionPageJson.results.forEach((block) => {
+      //先篩選type = paragraph
+      if (block.type !== "paragraph") return;
+      //再來篩選是否有料
+      if (block.paragraph.rich_text.length === 0) return;
+      //再來是否可以分割
+      const divideResult = this.tryDivideStrBySymbol(
+        block.paragraph.rich_text[0].plain_text,
+        "/"
+      );
+      if (divideResult.isSuccess) {
+        const infos = this.transformPageBlockValueToInfos(divideResult.part2);
+        const note = new Note(divideResult.part1, infos);
+        Notes.push(note);
+      } else {
+        WrongBlocks.push(block.paragraph.rich_text[0].plain_text);
+      }
+    });
+
+    return { Notes: Notes, WrongBlocks: WrongBlocks };
+  }
+
+  //將PageBlockValue分解成info
+  transformPageBlockValueToInfos(blockValue) {
+    const infos = [];
+    this.divideStrByRegex(blockValue).forEach((subStr) => {
+      infos.push(this.makeInfoByRegex(subStr));
+    });
+    return infos;
+  }
+
+  //Calculation
+  tryDivideStrBySymbol(str, symbol) {
+    const index = str.indexOf(symbol);
+    if (index !== -1) {
+      const part1 = str.substring(0, index); // 沒有宣告 part1
+      const part2 = str.substring(index + 1); // 沒有宣告 part2
+      return { isSuccess: true, part1: part1, part2: part2 };
+    } else {
+      return { isSuccess: false };
+    }
+  }
+
+  divideStrByRegex(str) {
+    const regex = /[^@~&]+|[@~&]\{[^}]*\}/g;
+    return str.match(regex);
+  }
+
+  makeInfoByRegex(str) {
+    const regexExample = /^@\{(?<content>.*)\}$/;
+    const regexNotice = /^~\{(?<content>.*)\}$/;
+    const regexReference = /^&\{(?<content>.*)\}$/;
+    let match = "";
+
+    match = str.match(regexExample);
+    if (match && match.groups?.content !== undefined) {
+      return new Info(Info.InfoType.EXAMPLE, match.groups.content);
+    }
+
+    match = str.match(regexNotice);
+    if (match && match.groups?.content !== undefined) {
+      return new Info(Info.InfoType.NOTICE, match.groups.content);
+    }
+
+    match = str.match(regexReference);
+    if (match && match.groups?.content !== undefined) {
+      return new Info(Info.InfoType.REFERENCE, match.groups.content);
+    }
+
+    return new Info(Info.InfoType.TEXT, str);
+  }
+
+  //return notionPageJson
+  async fetchNotionPageJson(token, pageId) {
     const apiUrl = `https://api.notion.com/v1/blocks/${pageId}/children?page_size=100`;
 
     try {
@@ -44,4 +124,27 @@ class NoteMaker {
       return null;
     }
   }
+}
+
+//NOTE: NOTE不知道來源類型，也不知道如何解析
+
+class Note {
+  constructor(key, infos) {
+    this.key = key;
+    this.infos = infos;
+  }
+}
+
+class Info {
+  constructor(type, content) {
+    this.type = type;
+    this.content = content;
+  }
+
+  static InfoType = Object.freeze({
+    TEXT: "Text",
+    REFERENCE: "Reference",
+    NOTICE: "Notice",
+    EXAMPLE: "Example",
+  });
 }
